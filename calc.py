e48bb8161fb455d7450a5c672ee9bf3fae6ce43f
programmer-88 <ianmbayyz@gmail.com>
2023-12-10 03:05:22 +0300

    added all arithmetic functions

--- calc.py
+++ calc.py
@@ -3,15 +3,19 @@ from engine.funcs import *
 from tkinter import *
 from engine.printer import *
 
+def exec_func():
+    t_out_1.delete(1.0, "end")
+    print(arithmetic_check(en_in_1.get()))
+    t_out_1.insert(END, en_in_1.get())
 
 window = Tk()
-
-b1 = Button(window, text="execute", command = arithmetic_check)
-b1.grid(row = 0, column = 0)
 
 en_in_1 = StringVar()
 en1 = Entry(window, textvariable = en_in_1)
 en1.grid(row = 0, column =1)
+
+b1 = Button(window, text="execute", command = exec_func)
+b1.grid(row = 0, column = 0)
 
 t_out_1 = Text(window, height =1, width = 40)
 t_out_1.grid(row = 1, column = 0, columnspan = 2)
--- engine/printer.py
+++ engine/printer.py
@@ -1,8 +1,43 @@
 #!/usr/bin/python3
 
 """Decides that the given arithmetic expression is"""
+from engine.funcs import *
+
 
 def arithmetic_check(args):
     """executes the given arithmetic expression"""
-    print("test successful")
+
+    items =args.split()
+
+    operand1= items[0]
+    operator= items[1]
+    operand2= items[2]
+
+    try: 
+        operand1 = int(operand1)
+        operand2 = int(operand2)
+
+    except ValueError:
+        raise ValueError("Invalid operand")
+    
+    if operator == "+":
+        return add(operand1, operand2)
+    
+    elif operator == "-":
+        return subtract(operand1, operand2)
+    
+    elif operator == "*":
+        return multiply(operand1, operand2)
+    
+    elif operator == "/":
+        return divide(operand1, operand2)
+    
+    elif operator == "**":
+        return square(operand1)
+    
+    elif operator == ")":
+        return square_root(operand1)
+    
+
+
     
